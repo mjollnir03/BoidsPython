5ebb3be735a6b7b50b66a1f5245338f44c676648
Ellmaer Ranjber <ellmaer2003@gmail.com>
2024-10-31 18:19:00 -0700

    create settings.py

---
+++ settings.py
@@ -0,0 +1,12 @@
+
+# MODULE IMPORTS
+import pygame 
+from pygame.locals import * 
+import math
+import numpy
+import threading 
+import random
+
+# COLORS
+BOID_COLOR = (255, 255, 255)
+SCREEN_BACKGROUND_COLOR = (0,0,0)
