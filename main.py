13064cc9b8ede99c7ccea62dd6405a25072e86b9
Ellmaer Ranjber <ellmaer2003@gmail.com>
2024-11-07 17:17:59 -0800

    Update main.py
    -instead of manually drawing agents, the boid class is used to make an instance of the class with an agent object
    -the agent object is used to display

--- main.py
+++ main.py
@@ -1,59 +1,39 @@
 from settings import *
 from boid import Boid
 
-# Initialize pygame
-pygame.init()
 
 
-#icon = pygame.image.load()
-pygame.display.set_caption("Flocking Simulation")
+agent1 = Boid(25, (255,0,0), (WIDTH//2, HEIGHT//2))
+agent1.calculate_dimensions()
 
-# Set up display information and print the current screen resolution
-displayInfo = pygame.display.Info()
-print("Screen resolution:", displayInfo.current_w, "x", displayInfo.current_h)
 
-# Set up screen and clock
-screen = pygame.display.set_mode((640, 480), HWSURFACE | DOUBLEBUF | RESIZABLE | DOUBLEBUF)  # Initial screen size 640x480, resizable
-clock = pygame.time.Clock()  # Create a clock to control frame rate
-
-width, height = 640, 480
-
-# Create a surface for the triangle
-triangle_surface = pygame.Surface((100, 100), pygame.SRCALPHA)  # 200x200 with transparency
-triangle_points = [(50, 0), (100, 100), (0, 100)]  # Centered triangle within the surface
-pygame.draw.polygon(triangle_surface, (255,0,0), triangle_points)
 
 angle = 0.0
 
-# Run window
-running = True
-while running:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
         elif event.type == VIDEORESIZE:
-            width, height = event.size  # Get new width and height from the resize event
-            if width < 600:  # Enforce minimum width of 600
-                width = 600
-            if height < 400:  # Enforce minimum height of 400
-                height = 400
-            screen = pygame.display.set_mode((width, height), HWSURFACE | DOUBLEBUF | RESIZABLE)  # Resize screen
+            WIDTH, HEIGHT = event.size  # Get new width and height from the resize event
+            if WIDTH < 600:  # Enforce minimum width of 600
+                WIDTH = 600
+            if HEIGHT < 400:  # Enforce minimum height of 400
+                HEIGHT = 400
+            SCREEN = pygame.display.set_mode((WIDTH, HEIGHT), HWSURFACE | DOUBLEBUF | RESIZABLE)  # Resize screen
 
     # Fill the screen with black (background color)
-    screen.fill(SCREEN_BACKGROUND_COLOR)
+    SCREEN.fill(SCREEN_BACKGROUND_COLOR)
     
-    rotated_triangle = pygame.transform.rotate(triangle_surface, angle)
+    
 
     # Draw Tringle 
-    screen.blit(rotated_triangle, rotated_triangle.get_rect(center=(width/2,height/2)))
-    
-    angle += 5.0
+    agent1.draw_boid(SCREEN)
+
     # Update display
     pygame.display.flip()
+    print(pygame.mouse.get_pos())
 
     # Limit frames per second
-    clock.tick(60)
-
-# Quit pygame after closing window
-pygame.quit()
+    CLOCK.tick(60)
