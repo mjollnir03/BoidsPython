235c21a1b1d67d39db44da70d656ec6ec414369d
Ellmaer Ranjber <ellmaer2003@gmail.com>
2024-11-17 12:29:01 -0800

    added getters/setters

--- boid.py
+++ boid.py
@@ -6,8 +6,8 @@ class Boid:
         self.__angle = 0
         self.__size = size
         self.__color = color
-        self.__position = Vector2(position)  # Use Vector2 for position
-        self.__direction = Vector2(0, -1)  # Initially up
+        self.__position = Vector2(position)
+        self.__direction = Vector2(0, -1)  # Initially pointing up
         self.__speed = 0  # Initial speed
         self.__calculate_dimensions()
 
@@ -28,7 +28,19 @@ class Boid:
         # Update position based on the direction and speed
         self.__position += self.__direction * self.__speed
 
-    def update_angle(self, angle_offset: int) -> None:
+    def set_position(self, position: Tuple[float, float]) -> None:
+        self.__position = Vector2(position)
+
+    def get_direction(self) -> Vector2:
+        return self.__direction
+
+    def get_position(self) -> Vector2:
+        return self.__position
+
+    def get_angle(self) -> float:
+        return self.__angle
+
+    def update_angle(self, angle_offset: float) -> None:
         # Adjust angle and rotate direction vector
         self.__angle += angle_offset
         self.__angle %= 360  # Keep angle within 0-359 degrees
@@ -42,7 +54,7 @@ class Boid:
         # Draw the rotated surface to the screen
         screen.blit(rotated_surface, rotated_rect)
 
-        # Optional 
+        # Optional: Draw a rectangle around the boid for debugging
         pygame.draw.rect(screen, (255, 0, 0), rotated_rect, 1)
 
     def display_boid_metrics(self) -> None:
@@ -52,8 +64,18 @@ class Boid:
         print(f"Current Speed: {self.__speed}")
         print()
 
-    def get_speed(self) -> int:
+    def get_speed(self) -> float:
         return self.__speed
 
-    def set_speed(self, speed) -> None:
+    def set_speed(self, speed: float) -> None:
         self.__speed = speed
+
+    # Additional methods if needed
+    def separation(self) -> None:
+        pass
+
+    def alignment(self) -> None:
+        pass
+
+    def cohesion(self) -> None:
+        pass
