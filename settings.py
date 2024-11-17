ae7ce33c97503528676ef6f6851ee6221696cf3a
Ellmaer Ranjber <ellmaer2003@gmail.com>
2024-11-17 12:30:27 -0800

    added extra global variables

--- settings.py
+++ settings.py
@@ -9,6 +9,13 @@ import sys
 from typing import List, Tuple
 from pygame.math import Vector2
 
+NUM_BOIDS = 500
+VISUAL_RANGE = 50
+PROTECTED_RANGE = 10
+AVOID_FACTOR = 0.05
+ALIGN_FACTOR = 0.05 
+COHESION_FACTOR = 0.0005
+
 # PYGAME INIT
 pygame.init()
 pygame.font.init()
@@ -24,6 +31,8 @@ FPS_FONT = pygame.font.SysFont("Arial" , 18 )
 pygame.display.set_caption("Flocking Simulation") 
 DISPLAY_INFO = pygame.display.Info()
 print("Screen resolution:", DISPLAY_INFO.current_w, "x", DISPLAY_INFO.current_h)
+
+SCREEN_MARGIN = 100
 
 WIDTH, HEIGHT = 640, 480
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT), HWSURFACE | DOUBLEBUF | RESIZABLE)
