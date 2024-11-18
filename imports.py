d35193a41e4a6932ec7126913c37f44b43edb229
Ellmaer <ellmaer2003@gmail.com>
2024-11-17 20:57:35 -0800

    create imports.py
    -decided to have 1 module to import everything used for this project
    -redundant but still, time saving

---
+++ imports.py
@@ -0,0 +1,15 @@
+# MODULE IMPORTS
+import pygame
+from pygame.locals import *
+from math import ceil
+import numpy
+import threading
+import random
+import sys
+from typing import List, Tuple
+from pygame.math import Vector2
+
+
+# PYGAME INIT
+pygame.init()
+pygame.font.init()
