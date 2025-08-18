ba3ba3033e7b7d6732c1dcdb23342accf9efa0d5
Ellmaer Ranjber <ellmaer2003@gmail.com>
2025-08-18 11:31:37 -0700

    settings is now a class
    settings is a dataclass which gives some structure

--- settings.py
+++ settings.py
@@ -1,28 +1,29 @@
 from imports import *
 
-# BEHAVIOR STATS
-NUM_BOIDS = 500
-VISUAL_RANGE = 50
-PROTECTED_RANGE = 10
-AVOID_FACTOR = 0.05
-ALIGN_FACTOR = 0.05 
-COHESION_FACTOR = 0.0005
+@dataclass
+class Settings():
+    # BEHAVIOR STATS
+    NUM_BOIDS = 500
+    VISUAL_RANGE = 50
+    PROTECTED_RANGE = 10
+    AVOID_FACTOR = 0.05
+    ALIGN_FACTOR = 0.05 
+    COHESION_FACTOR = 0.0005
 
-# COLORS
-BOID_COLOR = (255, 255, 255)
-SCREEN_BACKGROUND_COLOR = (0,0,0)
+    # COLORS
+    BOID_COLOR = (255, 255, 255)
+    SCREEN_BACKGROUND_COLOR = (0,0,0)
 
-# FONTS
-FPS_FONT = pygame.font.SysFont("Arial" , 18 )
+    # FONTS
+    FPS_FONT = pygame.font.SysFont("Arial" , 18 ) 
 
-# DISPLAY SETUP
-# ICON = pygame.image.load("")
-pygame.display.set_caption("Flocking Simulation") 
-# pygame.display.set_icon()
-DISPLAY_INFO = pygame.display.Info()
+    # DISPLAY SETUP
+    # ICON = pygame.image.load("")
+    # pygame.display.set_icon()
+    pygame.display.set_caption("Flocking Simulation") 
+    DISPLAY_INFO = pygame.display.Info() 
 
-SCREEN_MARGIN = 25
+    SCREEN_MARGIN = 25
+    WIDTH, HEIGHT = 640, 480
 
-WIDTH, HEIGHT = 640, 480
-SCREEN = pygame.display.set_mode((WIDTH, HEIGHT), HWSURFACE | DOUBLEBUF | RESIZABLE)
-CLOCK = pygame.time.Clock()  # Create a clock to control frame rate
+    CLOCK = pygame.time.Clock()
